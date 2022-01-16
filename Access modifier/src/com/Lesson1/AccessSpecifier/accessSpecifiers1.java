f65eedfc2e8baf3c8eefa9c36b29b8eb22049b70
kaustubh54 <68216145+kaustubh54@users.noreply.github.com>
2022-01-16 17:13:46 +0530

    Add files via upload

---
+++ Access modifier/src/com/Lesson1/AccessSpecifier/accessSpecifiers1.java
@@ -0,0 +1,10 @@
+package com.Lesson1.AccessSpecifier;
+
+public class accessSpecifiers1 {
+public static void main(String[] args) {
+//default
+System.out.println("Dafault Access Specifier");
+defAccessSpecifier obj = new defAccessSpecifier(); 
+ obj.display(); 
+}
+}
